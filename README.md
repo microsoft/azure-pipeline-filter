cb7c85b2bf7d7e218d8d44bd3c65dc0dc42ca3dd
Qinzheng Sun <qinsu@microsoft.com>
2022-05-17 15:04:42 +0800

    init

--- README.md
+++ README.md
@@ -1,14 +1,91 @@
-# Project
+# Azure Pipeline Filter
 
-> This repo has been populated by an initial template to help get you started. Please
-> make sure to update the content to build a great experience for community-building.
+Tools for conditional azure pipeline execution. Currently only supports run triggerd by github pull request.
 
-As the maintainer of this project, please make a few updates:
+## Usage
 
-- Improving this README.MD file to provide a great experience
-- Updating SUPPORT.MD with content about this project's support experience
-- Understanding the security reporting process in SECURITY.MD
-- Remove this section from the README
+The script `src/index.js` will check if current pipeline run matches following patterns. If **ANY** following checks failed, the output pipeline variable `skipsubsequent` variable will be set to **false**. Otherwise it will be set to **true**.
+
+The script should be put in a seperated job / stage and following jobs / stages could use the output variable to judge if it should be skipped.
+
+```yaml
+stages:
+- stage: Filter
+  jobs:
+  - job: Check
+    steps:
+    - task: NodeTool@0
+      inputs:
+        versionSpec: '16.x'
+    - script: npm ci
+      displayName: NPM Install
+    - script: node src/main.mjs
+      env:
+        FILTER_GITHUBPAT: $(filter.githubPAT)
+      name: execution
+      displayName: Execution
+- stage: Main
+  dependsOn: Check
+  # dependencies.$(StageName).outputs['$(JobName).$(TaskName).$(VariableName)']
+  condition: and(succeeded(), ne(dependencies.Filter.outputs['Check.execution.skipsubsequent'], 'true'))
+  jobs:
+  - job: Main
+    steps:
+    - script: echo end
+      displayName: 'Run main test'
+```
+
+## Checks
+
+### Basic Check
+
+Pipeline run should be triggerd by github pull request.
+
+### Modified Files Check
+
+Check if all modified files do **NOT** match the glob patterns provided by pipeline variable `filter.modified.globs`.
+
+File is compared (git diff -stat) between current commit and pr target branch.
+
+### Pull Request Body Check
+
+The script will try to find the task list under the heading provided by pipeline variable `filter.prbody.heading` from the pull request markdown body.
+
+If the task list is not found, continue (skip).
+
+If **ANY** selected options match provided index (`filter.prbody.optionIndex`) / value (`filter.prbody.optionValue`), run following tests.
+
+## Pipeline Variables
+
+### [PR body check] `filter.githubPAT`
+
+Github personal access token used to access the pr body. Could be ignored if the repo is public.
+
+It should be a secret variable and need to be exposed to the script explicitly.
+
+```yaml
+- script: node src/main.mjs
+  env:
+    FILTER_GITHUBPAT: $(filter.githubPAT)
+```
+
+### [PR body check] `filter.prbody.heading`
+
+Heading used by pull request body check to locate the task list.
+
+### [PR body check] `filter.prbody.optionIndex` / `filter.prbody.optionValue`
+
+Option index (start from 0) / value to be compared with selected options in the located task list.
+
+### [File Changes Check] `filter.modified.globs`
+
+Comma seperated glob patterns. See [multimatch](https://github.com/sindresorhus/multimatch#globbing-patterns)'s doc for more information.
+
+### [Output] `skipsubsequent`
+
+Output variable. If following tests need to be skipped, it will be set to true.
+
+Please refer to [azure pipeline's doc](https://docs.microsoft.com/en-us/azure/devops/pipelines/process/conditions?view=azure-devops&tabs=yaml) for more information.
 
 ## Contributing
 
---
+++ src/main.mjs
@@ -0,0 +1,144 @@
+import got from 'got'
+import simpleGit from 'simple-git'
+import multimatch from 'multimatch'
+
+import { parsePullRequstBody } from './parsePullRequestBody.mjs'
+
+const SKIP_VARIABLE = 'skipsubsequent'
+
+const VARIABLES = {
+  githubPAT: 'filter.githubPAT',
+  markdownHeading: 'filter.prbody.heading',
+  markdownOptionIndex: 'filter.prbody.optionIndex',
+  markdownOptionValue: 'filter.prbody.optionValue',
+  fileChangeGlobs: 'filter.modified.globs'
+}
+
+const getPipelineVar = (pipelineVar) => {
+  const key = pipelineVar.replaceAll('.', '_').toUpperCase()
+  return process.env[key]
+}
+
+const getGithubPullRequestInfo = async () => {
+  const repoId = process.env.BUILD_REPOSITORY_ID
+  const prNumber = process.env.SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
+  const githubPAT = getPipelineVar(VARIABLES.githubPAT)
+
+  const prUrl = `https://api.github.com/repos/${repoId}/pulls/${prNumber}`
+  const opt = {}
+  if (typeof (githubPAT) === 'string' && githubPAT.length > 0) {
+    const auth = Buffer.from(`:${githubPAT}`).toString('base64')
+    opt.headers = {
+      Authorization: `Basic ${auth}`
+    }
+  }
+  const resp = await got(prUrl, opt).json()
+  return resp
+}
+
+const basicCheck = async () => {
+  console.log('[Basic Check]')
+  const repoProvider = process.env.BUILD_REPOSITORY_PROVIDER
+  console.log('Repo Provider:', repoProvider)
+  if (repoProvider !== 'GitHub') {
+    console.log(`Invalid repo provider ${repoProvider}, run following tests by default.`)
+    return true
+  }
+
+  const buildReason = process.env.BUILD_REASON
+  console.log('Build Reason:', buildReason)
+  if (buildReason !== 'PullRequest') {
+    console.log('Not triggered by pull request, run following tests by default.')
+    return true
+  }
+
+  return false
+}
+
+const prBodyCheck = async () => {
+  console.log('[Pull Request Body]')
+  const markdownHeading = getPipelineVar(VARIABLES.markdownHeading)
+  console.log('Hint Heading:', markdownHeading)
+  if (typeof(markdownHeading) !== 'string' || markdownHeading.length == 0) {
+    console.log('Hint heading not provided, skip')
+    return false
+  }
+  let selectedOptions = []
+  const prInfo = await getGithubPullRequestInfo()
+  const prBody = prInfo.body || ''
+  try {
+    selectedOptions = parsePullRequstBody(prBody, markdownHeading)
+    console.log('Selected Options:', selectedOptions)
+  } catch (e) {
+    console.log('Error when parsing pull request body, skip.')
+    console.log(e)
+    return false
+  }
+
+  const optIdx = parseInt(getPipelineVar(VARIABLES.markdownOptionIndex), '10')
+  const optValue = getPipelineVar(VARIABLES.markdownOptionValue)
+  console.log(`Target Option Index: ${optIdx}`)
+  console.log(`Target Option Value: ${optValue}`)
+
+  for (const [value, idx] of selectedOptions) {
+    if (value === optValue || idx === optIdx) {
+      console.log(`Option ${idx} "${value}" selected, run following tests`)
+      return true
+    }
+  }
+
+  console.log('Option not selected.')
+  return false
+}
+
+const fileChangeCheck = async () => {
+  console.log('[File Changes]')
+  const globs = getPipelineVar(VARIABLES.fileChangeGlobs)
+  if (typeof (globs) !== 'string' || globs.length === 0) {
+    console.log('Glob pattern not provided, skip')
+    return false
+  }
+  const patterns = globs.split(',')
+  console.log(`Glob patterns: ${globs}`)
+
+  const targetBranch = process.env.SYSTEM_PULLREQUEST_TARGETBRANCH
+  const git = simpleGit()
+  const summary = await git.diffSummary(`origin/${targetBranch}`)
+  const files = summary.files.map(x => x.file)
+
+  const result = multimatch(files, patterns)
+  if (result.length > 0) {
+    console.log('Following changes match provided patterns:')
+    for (const path of result) {
+      console.log(path)
+    }
+    console.log('Run following tests')
+    return true
+  }
+
+  console.log('No changes match provided patterns')
+  return false
+}
+
+const setOutputVariable = (key, value) => {
+  console.log(`Set variable: ${key} -> ${value}`)
+  console.log(`##vso[task.setvariable variable=${key};isoutput=true]${value}`)
+}
+
+const main = async () => {
+  const checks = [
+    basicCheck,
+    fileChangeCheck,
+    prBodyCheck
+  ]
+  for (const check of checks) {
+    const res = await check()
+    if (res) {
+      setOutputVariable(SKIP_VARIABLE, false)
+      return
+    }
+  }
+
+  setOutputVariable(SKIP_VARIABLE, true)
+}
+main()
